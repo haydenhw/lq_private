f0bff042841d8524caf8686570924fba171144c7
hayden7913 <hayden321@gmail.com>
2019-03-07 17:24:23 -0800

    edits logs

--- backend/routes/hardwareBrokerMulti.js
+++ backend/routes/hardwareBrokerMulti.js
@@ -244,15 +244,15 @@ function constructServerMessage(messageBackToServer, rewriteCmd) {
     var splitMessage = messageBackToServer.split(',');
     var mm = splitMessage.shift().trim();
     var srverData = gMCUPreambles[mm];
-    console.log()
-    console.log('**gMCUPreambles**')  
-    console.log(gMCUPreambles)
-    console.log()
-    console.log()
-    console.log('**mm**')
-    console.log(mm)
-    console.log()
-    console.log()
+    // console.log()
+    // console.log('**gMCUPreambles**')  
+    // console.log(gMCUPreambles)
+    // console.log()
+    // console.log()
+    // console.log('**mm**')
+    // console.log(mm)
+    // console.log()
+    // console.log()
     if (srverData !== undefined) {
         while (splitMessage.length) {
             var pair = splitMessage.shift().trim();
